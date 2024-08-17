3d7d9c907e62d8a5e5af71ae6991e367709f046a
Mihai Oltean <lte.mitza@gmail.com>
2024-08-18 02:57:46 +0300

    Added dark theme

--- src/components/navigationHeader/NavigationHeader.tsx
+++ src/components/navigationHeader/NavigationHeader.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './NavigationHeader.css'
 
 const NavigationHeader = () => {
+
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+  };
+
   return (
     <header>
         <h1>Calendar</h1>
-        <i>Settings</i>
+        <i onClick={toggleTheme}>Settings</i>
     </header>
   )
 }
